27c7ff1d9dd110c73135e9317d1615352b3b7448
bpurinton <ben.purinton@gmail.com>
2022-10-08 12:13:48 +0200

    scripts ready

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 This repository is associated with:
 
-  _Purinton, B.; Mueting, A.; Bookhagen, B. [TITLE]. Remote Sensing. 2022. In preparation._
+  _Purinton, B.; Mueting, A.; Bookhagen, B. Image Texture as Quality Indicator for Optical DEM Generation: Geomorphic Applications in the Arid Central Andes. Remote Sensing. 2022. In review._
+
+
+**BEFORE YOU BEGIN PROCESSING READ ALL INSTRUCTIONS HERE:**
 
 Prior to running the Ames Stereo Pipeline on the SPOT6 images, we convert them from their native, tiled format with external Rational Polynomial Coefficient (RPC) files, to a GeoTiff format with the RPC data contained in the file header using the following GDAL command. Since Ames also needs the original *.xml RPC file for processing purposes to be fed into the pipeline at various steps, we also rename this file to match our GeoTiff filename. These commands are run in each of the three directories containing the A, B, and C tristereo images:
 
@@ -20,7 +23,16 @@ gdal_translate DIM_SPOT6_*.XML C.tif -co TILED=YES
 cp RPC_SPOT*_*.XML C.XML
 ```
 
-We note that the `ames_spot_processing.sh` file contains these preprocessing commands.
+Once this is done, you should generate a directory with a shortname for the area you are working in (e.g., for us it was `Pocitos`), and place the `A.tif`, `B.tif`, `C.tif` and `A.XML`, `B.XML`, `C.XML` files in a subdirectory called `merged_tiles` (i.e., `Pocitos/merged_tiles/`).
+
+Prior to running `ames_spot_processing.sh` you need to place the script in the directory of your SPOT processing (e.g., `Pocitos/ames_spot_processing.sh`), make sure you are inside this directory at the command line (e.g., `cd /path/to/Pocitos`), and make sure you have donwloaded and installed the Ames Stereo Pipeline and you can access all commands from your current command line.
+
+Once you have completed these steps, the processing parameters that you may wish to change are on line numbers `12-39` in the `ames_spot_processing.sh` shell script. Descriptions are provided inline, but we note the **alignment DEM at line `18`**. You will need to make a new subdirectory called `Alignment_DEMs` (e.g., `Pocitos/Alignment_DEMs`), and in there you should put a coarse (i.e., 30 m) DEM that will be used for co-registration and map-projection (Steps 5-7 in our publication Section 3.2.3). It is up to the user to source and appropriate DEM for this step, but we recommend the [Copernicus](https://portal.opentopography.org/raster?opentopoID=OTSDEM.032021.4326.3).
+
+**Once you have completed _ALL_ of the above pre-processing steps you are ready to run the shell script!**
+
+Output processing logs will be found in the `asp_logs/` subdirectory (e.g., `Pocitos/asp_logs/`) and all DEM results will be found in the `asp_out/` subdirectory (e.g., `Pocitos/asp_out/`).
+
 
 # SPOT6 A scene prior to and following map-projection
 ![SPOT6 A scene prior to and following map-projection.](mapproject.gif)
--- ames_spot_processing.sh
+++ ames_spot_processing.sh
@@ -1,42 +1,48 @@
 #!/usr/bin/env bash
 
-# set some parameters for part one
-rawRes=0.000014532898450 # pixel resolution in arc seconds
+# See Purinton, Mueting, and Bookhagen (2022) Remote Sensing for description
+# of processing steps in Section 3.2.3. Note that STEP 1 (GeoTiff conversion)
+# is done as a separate first step (see GitHub README: https://github.com/UP-RS-ESP/Ames-Stereo-Pipeline-SPOT).
+
+#############################################
+###### Here are the parameters to set #######
+#############################################
+
+# Name, resolution and alignment parameters
+area_name=Pocitos # shortname to give to the output DEM (same as subdirectory name you should be inside of)
+rawRes=0.000014532898450 # pixel resolution in degrees
 rawRes_meters=1.5 # pixel resolution in meters
 multiplier=2 # multiple of raw resolution for output DEMs
-resOut=$(echo $multiplier $rawRes | awk '{printf "%4.15f\n",$1*$2}')
-resOut_meters=3 # approximate output resolution in meters
+resOut=$(echo $multiplier $rawRes | awk '{printf "%4.15f\n",$1*$2}') # output resolution in degrees
+resOut_meters=$(echo $multiplier $rawRes_meters | awk '{printf "%4.15f\n",$1*$2}') # approximate output resolution in meters
+dem_name=Alignment_DEMs/cop1_s26w68_s25w66_wgs84.tif # DEM for alignment including path
 
-# add the alignment dem if it doesn't exist
-dem_name=/raid-manaslu/benp/SPOT/TDM_COP_DEMs/cop1_s26w68_s25w66_wgs84.tif # set the DEM name for alignment
-
-# Stereo Correlation parameters for both outputs
-# https://groups.google.com/g/ames-stereo-pipeline-support/c/nZf71YhJhuE/m/36UNaXPNCAAJ
-corr_mem_limit_mb=14336 # 15000 71680
-corr_tile_size=6400 # 512 1024 2048 3200 6400
+# Stereo Correlation parameters
+# for correlation memory limit and tile size discussion, see:
+    # https://groups.google.com/g/ames-stereo-pipeline-support/c/nZf71YhJhuE/m/36UNaXPNCAAJ
+corr_mem_limit_mb=14336 # correlation memory limit, decrease for less powerful machines
+corr_tile_size=6400 # correlation tile size, decrease for less powerful machines
 spr=2 # subpixel refinement; typically use Bayes EM (2) but there are some other options
 
-# Loop through some pair combinations
+# image pairs to loops through (AB, AC, BC)
 is=( A A B )
 js=( B C C )
 
-# BM
-bmCKernels=( 15 25 35 )
-bmSKernels=( 25 35 45 )
-bmCM=2 # for very large ck>9, need to use cost mode 2 (NCC); normally best to use 3 (Census!) or 4 (ternary census)
+# BM parameters
+bmCKernels=( 15 25 35 ) # correlation kernel sizes
+bmSKernels=( 25 35 45 ) # subpixel kernel sizes corresponding to CKs above
+bmCM=2 # cost mode, for very large ck>9, need to use cost mode 2 (NCC); otherwise best to use 3 (census) or 4 (ternary census)
 
-# MGM
-mgmCKernels=( 7 9 )
-mgmSKernels=( 15 21 )
-mgmCM=4 # for very large ck>9, need to use cost mode 2 (NCC); normally best to use 3 (Census!) or 4 (ternary census)
+# MGM parameters
+mgmCKernels=( 5 7 9 ) # correlation kernel sizes
+mgmSKernels=( 9 15 21 ) # subpixel kernel sizes corresponding to CKs above
+mgmCM=4 # cost mode, for very large ck>9, need to use cost mode 2 (NCC); otherwise best to use 3 (census) or 4 (ternary census)
 
-# shortname added to output DEMs to identify the study area
-area_name=Pocitos
 
-# move into directory
-cd /raid-manaslu/benp/SPOT/*${area_name}*/
-# cd /raid-manaslu/benp/SPOT/SPOT*_Toro/
-# area_name=Toro
+#############################################
+############ Begin processing ###############
+#############################################
+
 
 echo ""
 echo $area_name
@@ -50,32 +56,20 @@ echo ""
 echo ${i}${j}${k}
 echo ""
 
-# make some directories
+# make some sub directories
 mkdir -p asp_logs/
 mkdir -p asp_out/dems/
 
-# Prepare the images
-if [ ! -e merged_tiles/${i}.XML ]
-then
-  echo "merging tiles"
-  mkdir -p merged_tiles
-  for f in $i $j $k; do
-    gdal_translate PROD_SPOT*_001/VOL_SPOT*_001_${f}/IMG_SPOT*_P_001_${f}/DIM_SPOT*_*.XML merged_tiles/${f}.tif -co TILED=YES -co BIGTIFF=IF_SAFER
-    cp PROD_SPOT*_001/VOL_SPOT*_001_${f}/IMG_SPOT*_P_001_${f}/RPC_SPOT*_*.XML merged_tiles/${f}.XML
-  done
-fi
-echo "tiles merged"
-
-# bundle adjust on clip (no GCPs)...
-if [ ! -e asp_logs/ba.${i}${j}${k}_NOgcp ]
+# bundle adjust on clip
+if [ ! -e asp_logs/ba.${i}${j}${k} ]
 then
   echo "bundle adjusting"
   bundle_adjust -t rpc \
       merged_tiles/${i}.tif \
       merged_tiles/${j}.tif \
       merged_tiles/${k}.tif \
-      -o asp_out/ba_${i}${j}${k}_NOgcp/${i}${j}${k}_NOgcp --tif-compress Deflate --threads 18 \
-      --max-iterations 500 --datum WGS_1984 | tee asp_logs/ba.${i}${j}${k}_NOgcp
+      -o asp_out/ba_${i}${j}${k}/${i}${j}${k} --tif-compress Deflate --threads 18 \
+      --max-iterations 500 --datum WGS_1984 | tee asp_logs/ba.${i}${j}${k}
 fi
 echo "bundle adjusted"
 
@@ -84,6 +78,7 @@ if [ ! -e asp_out/dems/preliminary-DEM.tif ]
 then
     echo "generate asp_out/dems/preliminary-DEM.tif"
 
+    # initial stereo correlation on non map-projected images
     parallel_stereo --stereo-algorithm asp_bm \
       --xcorr-threshold 2 \
       --cost-mode 2 \
@@ -92,11 +87,12 @@ then
       --corr-tile-size ${corr_tile_size} \
       --corr-memory-limit-mb ${corr_mem_limit_mb} \
       --subpixel-mode 2 \
-      --bundle-adjust-prefix asp_out/ba_${i}${j}${k}_NOgcp/${i}${j}${k}_NOgcp \
+      --bundle-adjust-prefix asp_out/ba_${i}${j}${k}/${i}${j}${k} \
       merged_tiles/${i}.tif \
       merged_tiles/${k}.tif \
       asp_out/dems/stereo_preliminary/preliminary | tee asp_logs/preliminary.stereo.log
 
+    # generation of low-quality alignment DEM
     point2dem --t_srs "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" \
       --tr $resOut --threads 0 --nodata-value -9999 \
       asp_out/dems/stereo_preliminary/preliminary*-PC.tif \
@@ -113,7 +109,7 @@ then
       merged_tiles/${j}.tif \
       merged_tiles/${k}.tif \
       --initial-transform asp_out/dems/align/preliminary-DEM_to_COP1-transform.txt \
-      --input-adjustments-prefix asp_out/ba_${i}${j}${k}_NOgcp/${i}${j}${k}_NOgcp \
+      --input-adjustments-prefix asp_out/ba_${i}${j}${k}/${i}${j}${k} \
       --apply-initial-transform-only \
       -o asp_out/dems/ba/preliminary-DEM_to_COP1 --tif-compress Deflate --threads 18 --datum WGS_1984 \
        | tee asp_logs/preliminary.ba.log
@@ -127,7 +123,7 @@ then
       merged_tiles/${f}_DD_WGS84_${rawRes_meters}m.tif
     done
 
-    # for some reason I have to do this
+    # for some reason you need to change the filenames to get them to work in the next steps
     for f in $i $j $k; do
     cp asp_out/dems/ba/preliminary-DEM_to_COP1-${f}.adjust \
        asp_out/dems/ba/preliminary-DEM_to_COP1-${f}_DD_WGS84_${rawRes_meters}m.adjust
@@ -137,7 +133,7 @@ fi
 echo "asp_out/dems/preliminary-DEM.tif exists!"
 
 ################################
-######## BM - All combos ##################
+######## BM - All combos #######
 ################################
 # loop through the DEMs
 for idx in "${!is[@]}"; do
@@ -181,7 +177,7 @@ for idx in "${!is[@]}"; do
 done
 
 ################################
-######## MGM - All combos ##################
+######## MGM - All combos ######
 ################################
 # loop through the DEMs
 for idx in "${!is[@]}"; do
